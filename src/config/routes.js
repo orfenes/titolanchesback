7f447e1e78b76aa4bb699b02c25069d67af8c2fb
Rodrigo Fernandes Orfenes de Castro <you@example.com>
2020-05-22 12:50:48 -0300

    create auth the app

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,27 @@
 
 const express = require('express');
+const auth = require('./auth');
 
 module.exports = (app) => {
-  const router = express.Router();
+  /*
+  * Rotas privadas
+  */
+  const protectedApi = express.Router()
+  app.use('/api', protectedApi)
+
+  protectedApi.use(auth)
 
   const Client = require('../api/client/clientService');
-  Client.register(app, '/api/clients');
+  Client.register(protectedApi, '/clients');
 
+  /*
+  * Rotas abertas
+  */
+ const openApi = express.Router()
+ app.use('/oapi', openApi);
+
+ const AuthService = require('../api/user/AuthService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 }
